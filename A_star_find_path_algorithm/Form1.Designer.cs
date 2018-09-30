64580c20e10a0f4c56983fc1e85e12f236a5260b
a97410985new <a97410985new@gmail.com>
2018-09-30 14:51:41 +0800

    基本二維陣列按鈕配置

--- A_star_find_path_algorithm/Form1.Designer.cs
+++ A_star_find_path_algorithm/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_setup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_setup
+            // 
+            this.btn_setup.Location = new System.Drawing.Point(871, 13);
+            this.btn_setup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_setup.Name = "btn_setup";
+            this.btn_setup.Size = new System.Drawing.Size(94, 31);
+            this.btn_setup.TabIndex = 0;
+            this.btn_setup.Text = "set up";
+            this.btn_setup.UseVisualStyleBackColor = true;
+            this.btn_setup.Click += new System.EventHandler(this.btn_setup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(978, 1004);
+            this.Controls.Add(this.btn_setup);
+            this.Font = new System.Drawing.Font("新細明體", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_setup;
     }
 }
 
--- A_star_find_path_algorithm/Form1.cs
+++ A_star_find_path_algorithm/Form1.cs
@@ -12,9 +12,35 @@ namespace A_star_find_path_algorithm
 {
     public partial class Form1 : Form
     {
+        const int ROW = 10;
+        const int COL = 10;
+        const int gridWid = 70;
+        const int gridHgt = 70;
+        const int UpperLeftCornerX = 30;
+        const int UpperLeftCornerY = 30;
+
+
+        Button[,] BtnMap = new Button[COL, ROW]; // x先，y後
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_setup_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < COL; i++)
+            {
+                for (int j = 0; j < ROW; j++)
+                {
+                    BtnMap[i, j] = new Button();
+                    Controls.Add(BtnMap[i, j]);
+                    BtnMap[i, j].Location = new Point(UpperLeftCornerX+i*gridWid, UpperLeftCornerY+j*gridHgt);
+                    BtnMap[i, j].Width = gridWid;
+                    BtnMap[i, j].Height = gridHgt;
+                    BtnMap[i, j].Text = "12  34\n\n56";
+                }
+            }
+            btn_setup.Enabled = false;
         }
     }
 }
